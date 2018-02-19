f82d3e050bf0d5c4ef9305e363ca91bcb35ac1c4
William Crump <ricky@Williams-MacBook-Pro.local>
2018-02-19 15:11:22 -0500

    Functionality for parsing log files based on matchers works.

--- src/LogParser.hs
+++ src/LogParser.hs
@@ -1,17 +1,42 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module LogParser where
 
 import System.IO
 import Data.List
+import Data.Aeson
+import GHC.Generics
+import qualified Data.ByteString.Lazy as B
 
 type LogFilePath = String
 type ConfigFilePath = String
 type Matcher = String
 
+data Rule = Rule {
+    matcher :: Matcher
+} deriving (Eq, Generic, Show)
+
+data ConfigRules = ConfigRules {
+    rules :: [Rule]
+} deriving (Eq, Generic, Show)
+
+emptyConfig :: ConfigRules
+emptyConfig = ConfigRules [Rule ""]
+
+instance FromJSON Rule
+instance FromJSON ConfigRules
+
 parse :: LogFilePath -> ConfigFilePath -> IO String
 parse l c = do 
+    configContents <- B.readFile c
     logContents <- readFile l
+    let parsedRules = case (decode configContents) of
+                        Nothing -> rules emptyConfig
+                        Just value -> rules value
     let line = lines logContents
-    return logContents
+    let output = foldl (\o m -> o ++ (parseWithMatcher (matcher m) line) ++ "\n") "" parsedRules
+    return output
 
 parseWithMatcher :: Matcher -> [String] -> String
 parseWithMatcher m xs = m ++ " = " ++ (show $ countLines m xs)
@@ -20,6 +45,7 @@ countLines :: Matcher -> [String] -> Int
 countLines _ [] = 0 
 countLines "" _ = 0
 countLines m (x:xs) = (count m x) + (countLines m xs)
+
 -- non-recursive verison, not sure how haskell handles tail optimization
 -- countLines m xs = foldl (\a b -> (count m b) + a) 0 xs
 
--- test/LogParserSpec.hs
+++ test/LogParserSpec.hs
@@ -11,14 +11,17 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "LogParser" $ do
+  describe "parse" $ do
     it "Should read a log file and a config file and return the number of matching lines that are found" $ do
-      parse "test/testfiles/log.log" "test/testfiles/config.cfg" `shouldReturn` "conreq = 2"
+      let expected = "conreq = 2\nRest Request = 2\n"
+      parse "test/testfiles/log.log" "test/testfiles/config.json" `shouldReturn` expected
 
-    it "Should take a line from a log and count the number of matching parameters" $
-      count "conreq" "conreq completed succesfully" `shouldBe` 1
+  describe "parseWithMatcher" $ do
+    it "Should return formated string output `matcher` = `count`" $
+      parseWithMatcher "conreq" ["conreq", "conreq"] `shouldBe` "conreq = 2"
 
-    it "Should take a list lines from a log and sum the total matching patameters" $
+  describe "countLines" $ do
+    it "Should take a list of lines from a log and sum the total matching parameters" $
       countLines "conreq" ["conreq", "conreq"] `shouldBe` 2
 
     it "Should return 0 when passed an empty list of lines" $
@@ -27,5 +30,6 @@ spec = do
     it "Should return 0 when passed an empty string as a matcher" $
       countLines "" ["conreq"] `shouldBe` 0
 
-    it "Should return formated string output `matcher` = `count`" $
-      parseWithMatcher "conreq" ["conreq", "conreq"] `shouldBe` "conreq = 2"
+  describe "count" $ do
+    it "Should take a line from a log and count the number of matching parameters" $
+      count "conreq" "conreq completed succesfully" `shouldBe` 1
