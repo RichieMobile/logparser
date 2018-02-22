c3f92cffa896747ecd8acd61738e23826331462f
RichieMobile <ricky@williams-mbp.strsoftware.com>
2018-02-22 08:37:53 -0500

    Started implementing time averaging.

--- src/Matchers/TimeAverager.hs
+++ src/Matchers/TimeAverager.hs
@@ -2,16 +2,36 @@ module Matchers.TimeAverager where
 
 import Data.Time
 import Data.Time.Format
+import Data.Time.Clock
+import qualified LogParser as L
 
 type BeginningComp = String
 type EndComp = String
 type TimeF = String
+type FileLines = String
+type Range = String
 
-calculateAverageTime :: BeginningComp -> EndComp -> TimeF -> String -> String -> UTCTime
+calculateAverageTime :: BeginningComp -> EndComp -> TimeF -> Range -> [FileLines] -> UTCTime
+calculateAverageTime bC eC f r lines = do
+    let bCMatches = filter (\x -> isMatch bC x) lines
+    let bCTimestamps = map (\x -> parseTimestampFromLine f 0 19 x)
+    let eCMatches = filter (\x -> isMatch eC x) lines
+    let eCTimestamps = map (\x -> parseTimestampFromLine f 0 19 x)
+    let timestampTuples = zip bCTimestamps eCTimestamps
+
+calculateAverageTimeWithTuples :: [(UTCTime, UTCTime)] -> UTCTime
+calculateAverageTimeWithTuples times = do
+    let timeDifferences = map (\(f,s) -> diffUTCTime f s)
+
+isMatch :: String -> String -> Bool
+isMatch matcher line = 
+    case count matcher line of
+        1 -> True
+        _ -> False
 
 parseTimestampFromLine :: TimeF -> Int -> Int -> String -> UTCTime
 parseTimestampFromLine format begining end line = do
-    let timestamp = take (end - begining) (take end line)
+    let timestamp = drop begining (take end line)
     let utcTimestamp = parseTimestamp format timestamp
     utcTimestamp
 
--- test/Matchers/TimeAveragerSpec.hs
+++ test/Matchers/TimeAveragerSpec.hs
@@ -12,10 +12,15 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "TimeAverager" $ do
-    it "Should be able to parse timestamp from log" $ do
+    it "Should be able to parse timestamp from beginning of log" $ do
         let dateLine = "2018-02-20 09:06:54 INFO: Yes Yes Y'all"
         let format = "%Y-%m-%d %T"
         show (parseTimestampFromLine format 0 19 dateLine) `shouldBe` "2018-02-20 09:06:54 UTC"
+
+    it "Should be able to parse timestamp from middle of log" $ do
+        let dateLine = "INFO: 2018-02-20 09:06:54 - Yes Yes Y'all"
+        let format = "%Y-%m-%d %T"
+        show (parseTimestampFromLine format 6 25 dateLine) `shouldBe` "2018-02-20 09:06:54 UTC"
 
     it "Should be able to parse timestamp with format" $ do
         let dateLine = "2018-02-20 09:06:54"
