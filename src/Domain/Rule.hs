5c323801d25c0eed6ddfb2334cff4fe058d08f41
RichieMobile <ricky@Williams-MacBook-Pro.local>
2018-02-24 21:10:42 -0500

    Removed commented code blocks.

--- src/Domain/Rule.hs
+++ src/Domain/Rule.hs
@@ -17,13 +17,6 @@ counter = "count"
 ratio = "ratio"
 avgTime = "avgTime"
 
--- data Rule = Rule {
---       matcher :: Matcher
---     , comparitor :: String
---     , processor :: String
---     , timestampFormat :: String
---     , range :: String
--- } deriving (Eq, Generic, Show)
 emptyRule :: Rule
 emptyRule = Rule [] [] []
 
@@ -39,7 +32,7 @@ instance Prs Rule where
 
 parseWithParsers :: Rule -> [String] -> String
 parseWithParsers rule lines = do
-  let ct = "Count Match\n" ++ (foldl (\x c -> x ++ (parse c lines)) "" (counters rule)) ++ "\n"
-  let rt = "Ratio Match\n" ++ (foldl (\x r -> x ++ (parse r lines)) "" (ratios rule)) ++ "\n"
-  let ta = "Average Time to Complete\n" ++ (foldl (\x t -> x ++ (parse t lines)) "" (timeAvgs rule))
-  "\n" ++ ct ++ "\n" ++ rt ++ ta
+  let ct = foldl (\x c -> x ++ (parse c lines)) "" (counters rule)
+  let rt = foldl (\x r -> x ++ (parse r lines)) "" (ratios rule)
+  let ta = foldl (\x t -> x ++ (parse t lines)) "" (timeAvgs rule)
+  ct ++ "\n" ++ rt ++ "\n" ++ ta
--- src/Matchers/Parser.hs
+++ src/Matchers/Parser.hs
@@ -1,14 +1,4 @@
 module Matchers.Parser where
 
--- import Matchers.TimeAverager
--- import Matchers.Counter
--- import Matchers.RatioCalculator
-
 class Prs a where
     parse :: a -> [String] -> String
-
--- parse :: a -> [String] -> String
--- parse (Counter m) lines = parseWithMatcher m lines
--- parse (RatioCalculator f s) lines = parseWithRatio f s lines
--- parse (AverageTimeParser b e f r) lines = parseAverageTime b e f r lines
--- parse _ lines = "Unknown Parser"
